9a62fc9c04b268ee655bf8bcff496104bfeb7628
Charles-Antoine Sohier <charles@medeo-health.com>
2018-01-25 18:09:40 +0100

    refactor(boilerplate): remove css and counter

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,9 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Home.css';
 
-type Props = {};
+type Props = {
+  className?: string
+};
 
 export default class Home extends Component<Props> {
   props: Props;
@@ -11,9 +11,8 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div>
-        <div className={styles.container} data-tid="container">
+        <div className={this.props.className}>
           <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
         </div>
       </div>
     );
